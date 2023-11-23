1b50449c1c47817080cbec7e1a17ce234329a0a0
john <john@gmail.com>
2023-11-23 21:02:59 +0500

    second commit changed in Main

--- src/java/Main.java
+++ src/java/Main.java
@@ -3,5 +3,8 @@ package src.java;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Git learning");
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
     }
 }
